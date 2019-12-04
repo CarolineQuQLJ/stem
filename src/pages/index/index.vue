a691dd67eec53136cb8c96cdf9c1d580862d6448
CarolineQuQLJ <1085348932@qq.com>
2019-12-04 15:39:00 +0800

    enter

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -21,33 +21,28 @@
         </i-grid-icon>
         <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item>
-  
-     <view class='title'>
-        <text class='cn'>猜你喜欢</text>
-        <text class='en'>Lovely</text>
-     </view>
      
     </i-grid>
     <i-panel title="今日热播">
         <view style="padding: 15px;">
-        <i-card  v-for="item in top" :key="item" i-class="split" :title="item.name" :extra="item.remark" :thumb="item.image">
+        <i-card  v-for="item in top" :key="item" i-class="split" :title="item.name" :thumb="item.image">
         <view slot="content">{{item.remark}}</view>
         <view slot="footer">{{item.address}}</view>
         </i-card>
 
     </view>
     </i-panel>
-    <i-panel :title="title_name">
+    <i-panel title="高分推荐">
     <view style="padding: 10px;">
-        <i-card i-class="split" title="标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <i-card i-class="split" title="我们诞生在中国" extra="额外内容" thumb="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575454117478&di=26037fee3b4a03851e52878076a7291c&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201607%2F20%2F20160720141506_aBjsU.thumb.700_0.jpeg">
         <view slot="content">内容不错</view>
-        <view slot="footer">尾部内容</view>
+        <view slot="footer">陆川</view>
         </i-card>
     </view>
     </i-panel>
     <i-panel title="猜你喜欢">
         <view style="padding: 10px;">
-        <i-card i-class="split" title="标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <i-card i-class="split" title="不惧风暴" extra="额外内容" thumb="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575454243632&di=5cc942c0924636a567b1ca0ea8d2faec&imgtype=0&src=http%3A%2F%2Fwww.sinaimg.cn%2Fent%2Fr%2Fm%2F2014-09-18%2FU4611P28T3D4211166F326DT20140918094514.jpg">
         <view slot="content">内容不错</view>
         <view slot="footer">尾部内容</view>
         </i-card>
@@ -69,10 +64,10 @@ export default {
         {title:"其它",image:"/static/images/5.png"}
       ],
       top:[
-        {name:"点名1",address:"地址1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍1"},
-        {name:"点名2",address:"地址2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
-        {name:"点名3",address:"地址3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
-        {name:"点名4",address:"地址4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
+        {name:"《Oceans海洋》",address:"BBC <法>雅克·贝汉、雅克·克鲁佐德",image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575453637872&di=e4763ddf1e9cb1c6c66818e058aae7ff&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F984baf97133e96eaed010374131571407d0240552887c-LREQgK_fw658",remark:"深入探索这个幽深而富饶的神秘世界、完整地呈现海洋的壮美辽阔。"},
+        {name:"《风味人间》",address:"腾讯视频 陈晓卿",image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575453905608&di=e071ae67ad543636a705711fe9413acb&imgtype=jpg&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20190416%2F0f7d2db6dd79440fa64d4d2c868697f3.jpeg",remark:"世界美食背后，有着浓浓的人情味，食物的迁徙背后，正有着移民迁徙的足印。"},
+        {name:"《Free Solo徒手攀岩》",address:"<美>金国威、伊丽莎白·柴·瓦萨赫伊",image:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575453993670&di=bb455c573cbc2d1ce6576794d94c2bbb&imgtype=0&src=http%3A%2F%2Fimage.thepaper.cn%2Fwww%2Fimage%2F15%2F966%2F526.jpg",remark:"美国攀岩大师亚历克斯·霍诺德（Alex Honnold）2017年6月3日无辅助徒手攻克美国约塞米蒂国家公园3000英尺高的酋长岩的全过程，共用时3小时56分钟。"},
+   
       ],
       imgUrls: [
            '/static/images/L1.jpg',
